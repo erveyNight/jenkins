537732b96c9371214e13701c23ceaa904f0d4b88
Matthew Wu <Matthew_96@163.com>
2019-10-08 07:50:18 +0800

    init

---
+++ src/main/java/jenkins/JenkinsApplication.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Wuah
+ * @Date: 2019-11-08 18:39
+ */
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+}
---
+++ src/main/java/jenkins/demo/action.java
@@ -0,0 +1,8 @@
+package jenkins.demo;
+
+/**
+ * @Author: Wuah
+ * @Date: 2019-11-08 18:40
+ */
+public class action {
+}
